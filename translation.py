4df3299ecd018b89d0e614760322066df368fce5
Arisu0000 <130484247+Arisu0000@users.noreply.github.com>
2023-11-22 12:07:00 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -48,9 +48,9 @@ ABOUT_TEXT = """
     
 `📝 Language:` [Python 3](https://www.python.org/)
 `🧰 Framework:` [Pyrogram](https://github.com/pyrogram/pyrogram)
-`👨‍💻 Developer:` [Dev](t.me/ask_admin001)
-`📢 Support:` [Talk Bot](https://t.me/ask_admin001)
-`🌐 Source Code:` [GitHub](https://github.com/kevinnadar22/URL-Shortener-V2/)
+`👨‍💻 Developer:` [Dev](t.me/profitlinks_admin)
+`📢 Support:` [Updates](https://t.me/profitlinks7)
+`🌐 Source Code:` [TG](t.me/PyroDEVBot)
 """
 
 
