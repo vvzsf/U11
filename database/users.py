d845b9997a20cd5b12b0a61fd69b2b02a8e2cf63
GH <130484247+vvzsf@users.noreply.github.com>
2023-12-31 17:46:12 +0530

    Update users.py

--- database/users.py
+++ database/users.py
@@ -12,7 +12,7 @@ async def get_user(user_id):
     if not user:
         res = {
             "user_id": user_id,
-            "method": "mdlinks",
+            "method": "mdisk",
             "shortener_api": None,
             "mdisk_api": None,
             "header_text": "",
