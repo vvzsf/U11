5f62c6d4ed45fb1cc35b4a9216784749bed4d42e
GH <130484247+vvzsf@users.noreply.github.com>
2024-01-07 17:07:37 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -380,12 +380,12 @@ async def create_server():
 async def set_commands(app):
     COMMANDS = [
         BotCommand("start", "Start For Own Earnings ✳️."),
-        BotCommand("api", "Set Urlshortx API ✨."),
-        BotCommand("header", "Set Header Text 🖍️."),
-        BotCommand("footer", "Set Footer Text 🖌️."),
-        BotCommand("me", "Know About You 🚶."),
-        BotCommand("banner_image", "Set Banner Image 📸."),
-        BotCommand("broadcast", "Admin Only 📣."),
+        BotCommand("api", "Set Urlshortx API ✨"),
+        BotCommand("header", "Set Header Text 🖍️"),
+        BotCommand("footer", "Set Footer Text 🖌️"),
+        BotCommand("me", "Know About You 🚶"),
+        BotCommand("banner_image", "Set Banner Image 📸"),
+        BotCommand("broadcast", "Admin Only 📣"),
     ]
 
     await app.set_bot_commands(COMMANDS)
